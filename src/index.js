fe1b8478d752f7d7cad228e2cf1266504e457a6d
Yunus Bahtiar <aboomarmoshab22@gmail.com>
2021-10-12 10:19:28 +0700

    static version

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles/styles.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+function Tweet() {
+    return (
+        <div className="tweet">
+            <Avatar />
+            <div className="content">
+                <Author />
+                <Time />
+                <Message />
+                <div className="buttons">
+                    <ReplyButton />
+                    <RetweetButton />
+                    <LikeButton />
+                    <MoreOptionsButton />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function Avatar() {
+    return (
+        <img src="https://www.gravatar.com/avatar/nothing" className="avatar" alt="avatar" />
+    );
+}
+
+function Message() {
+    return (
+        <div className="message">
+            This is less than 140 characters 👋
+        </div>
+    );
+}
+
+function Author() {
+    return (
+        <span className="author">
+            <span className="name">Yunus Bahtiar</span>
+            <span className="handle">@yunnix</span>
+        </span>
+    );
+}
+
+const Time = () => ( <span className="time">3h ago</span> );
+const ReplyButton = () => ( <i className="fa fa-reply reply-button" /> );
+const RetweetButton = () => ( <i className="fa fa-retweet retweet-button" />
 );
+const LikeButton = () => ( <i className="fa fa-heart like-button" /> );
+const MoreOptionsButton = () => ( <i className="fa fa-ellipsis-h more-options-button" /> );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Tweet />, document.querySelector('#root'));
